b8a5c5300137a10aae8e81a5a147472706a0e2d3
AslanMus <asmus01@mail.ru>
2024-05-11 12:48:55 +0300

    Ok

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -30,9 +31,18 @@ public class TestBase {
         Configuration.browserCapabilities = capabilities;
 
     }
+
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 
